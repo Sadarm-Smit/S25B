88ea958ccda17edcc39f9c756d5abb3d8b8cecab
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-10-20 23:30:02 +0700

    s

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
-    <title>Sovann</title>
 </head>
+<html>
 <body>
-    <?php 
-        echo "Hello World";
-    ?>
+
+<?php
+echo "My first PHP script!";
+?>
+
 </body>
 </html>
