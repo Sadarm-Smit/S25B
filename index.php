d93108b9eb3ff0be31432a91145a715d43f45120
Wolf3Team <112640442+Sadarm-Smit@users.noreply.github.com>
2022-10-21 22:29:03 +0700

    s

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-    $username = $_POST['username'];
-
-    $conn = new mysqli('localhost','root','','test');
-    if($conn->connect_error){
-        die('Connection Failed : '.$conn->connect_error);
-    }else {
-        $stmt = $conn->prepare("insert into registration(username)
-            values(?)")
-        $stmt->bind_param("s",$username);
-        $stmt->execute();
-        echo "registration Successfully...";
-        $stmt->close();
-        $conn->close();
-            
-    }
+    echo "Study " . $_GET['subject'];
 ?>
